5c56005e3ea6c7356deef1b8ea4399f36a5dbb11
ToluwalaseOkuwoga <toluwalaseokuwoga@yahoo.com>
2024-03-22 16:15:47 +0200

    Initial redesign

--- js/scripts.js
+++ js/scripts.js
@@ -28,3 +28,33 @@ function adjustBodyPadding() {
     const headerHeight = document.querySelector('header').offsetHeight;
     document.body.style.paddingTop = `${headerHeight}px`;
 }
+
+let currentCoverIndex = 0;
+const covers = [
+    {
+        img: "images/maincover1.jpg",
+        caption: "<em>On the Grid</em>, by Klaas Verplancke"
+    },
+    {
+        img: "images/latest_issue.jpg",
+        caption: "<em>Another Title</em>, by Another Artist"
+    },
+    {
+        img: "images/latest_issue.jpg",
+        caption: "<em>Third Title</em>, by Another Artist"
+    },    {
+        img: "images/latest_issue.jpg",
+        caption: "<em>Another One Title</em>, by Another Artist"
+    },
+];
+
+function navigateCover(direction) {
+    currentCoverIndex += direction;
+    if (currentCoverIndex >= covers.length) {
+        currentCoverIndex = 0; // Loop back to the first cover
+    } else if (currentCoverIndex < 0) {
+        currentCoverIndex = covers.length - 1; // Loop back to the last cover
+    }
+    document.getElementById("magazineCover").src = covers[currentCoverIndex].img;
+    document.querySelector(".cover-caption").innerHTML = covers[currentCoverIndex].caption;
+}

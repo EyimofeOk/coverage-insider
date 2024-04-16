aad982a3a2d0c7b27b9001b7256349d01f846f13
ToluwalaseOkuwoga <toluwalaseokuwoga@yahoo.com>
2024-04-16 09:10:46 +0300

    lightbox

--- js/scripts.js
+++ js/scripts.js
@@ -36,6 +36,10 @@ const covers = [
         caption: "<em>New Issue</em>"
     },
     {
+        img: "images/2.png",
+        caption: "<em>Oh Yeah, it's similar but this is PR?</em>"
+    },
+    {
         img: "images/4.png",
         caption: "<em>No one really knows what PR is</em>"
     },
@@ -83,3 +87,25 @@ document.addEventListener("DOMContentLoaded", function() {
       element.style.border = "2px solid red";
     }
   }); */
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("magazineCover");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
